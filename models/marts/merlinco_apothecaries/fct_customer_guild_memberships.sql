a04388fd56db58415525e72385f0cd1e854c8474
Jairus Martinez <114552516+jairus-m@users.noreply.github.com>
2026-07-24 09:57:59 -0700

    Refactor marts to use int w/ logic, not pure staging

--- models/marts/merlinco_apothecaries/fct_customer_guild_memberships.sql
+++ models/marts/merlinco_apothecaries/fct_customer_guild_memberships.sql
@@ -1,10 +1,2 @@
-select
-    membership_id,
-    customer_id,
-    guild_id,
-    membership_tier,
-    valid_from,
-    valid_to,
-    valid_from <= current_date
-        and (valid_to is null or valid_to >= current_date) as is_current_membership
-from {{ ref('stg_merlinco_guild_memberships') }}
+select *
+from {{ ref('int_merlinco_customer_guild_memberships') }}
